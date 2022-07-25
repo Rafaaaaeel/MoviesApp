2c44117c732833a5ebcbe97a514a1afa05b772f5
RafaelViaiOS <rafael.pires@viavarejo.com.br>
2022-07-25 12:01:36 -0300

    dw
    dwadwdaQ

--- MoviesTests/Search/MovieStoreTestCase.swift
+++ MoviesTests/Search/MovieStoreTestCase.swift
@@ -13,23 +13,14 @@ import XCTest
 class MovieStoreTestCase: XCTestCase{
 
     var sut: MovieStore!
-    var mockLoadURLAndDecode: MockLoandURLAndDecode!
     
     override func setUp() {
-        sut = MovieStore.shared
-        mockLoadURLAndDecode = MockLoandURLAndDecode(url: "", params: nil)
+        
     }
     
     override func tearDown() {
-        sut = nil
-        mockLoadURLAndDecode = nil
-    }
-    
-    func test() async throws{
         
-        let result = try await sut.searchMovie(query: "")
-        
-        XCTAssertThrowsError(MovieError.apiError)
     }
+
 }
 
